6d5b3b4644c28f7b9c145cefc73759d84ce2907a
Gabriel Kirsten <gabriel.kirsten@hotmail.com>
2019-03-27 19:35:49 -0400

    fixed manifest.json

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,13 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/personal-page/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/personal-page/' : '/',
+  pwa: {
+    name: 'My App',
+    themeColor: '#4DBA87',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+
+    // configure the workbox plugin
+    workboxPluginMode: 'InjectManifest',
+  }
 }
