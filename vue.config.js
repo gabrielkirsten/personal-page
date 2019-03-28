8b50534363307f78760db7528aa2b9609e92382a
Gabriel Kirsten <gabriel.kirsten@hotmail.com>
2019-03-28 07:15:43 -0400

    removed vue manifest config from vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,3 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/personal-page/' : '/',
-  pwa: {
-    name: 'My App',
-    themeColor: '#4DBA87',
-    msTileColor: '#000000',
-    appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: 'black',
-
-    // configure the workbox plugin
-    workboxPluginMode: 'InjectManifest',
-  }
 }
