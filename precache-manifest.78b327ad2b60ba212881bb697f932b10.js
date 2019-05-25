7307c8d9df869b3be74001ac9d72c865b2c1f315
Gabriel <gabriel.kirsten@hotmail.com>
2019-05-25 13:06:54 -0400

    0710e33

---
+++ precache-manifest.78b327ad2b60ba212881bb697f932b10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/personal-page/robots.txt"
+  },
+  {
+    "revision": "49fcd44cf843adb6a052",
+    "url": "/personal-page/js/chunk-vendors.93138f80.js"
+  },
+  {
+    "revision": "d34a79516f0decb1439b",
+    "url": "/personal-page/js/app.83eaf30f.js"
+  },
+  {
+    "revision": "3e2b5a1e2691403b28543e58af3828bd",
+    "url": "/personal-page/index.html"
+  },
+  {
+    "revision": "ca0cf5c58e744b42a8d53be67dcf0c48",
+    "url": "/personal-page/img/profile.ca0cf5c5.jpg"
+  },
+  {
+    "revision": "d34a79516f0decb1439b",
+    "url": "/personal-page/css/app.36145630.css"
+  }
+];
